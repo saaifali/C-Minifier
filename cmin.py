baf9815968c64a7fbdb3011dfcf581de458c3342
saaifali <crossfire.saif@gmail.com>
2017-03-30 23:49:47 +0530

    Output to a file

--- cmin.py
+++ cmin.py
@@ -9,37 +9,18 @@ def remove_blank_lines(lines):
             lines.pop(i)
     return lines
 
+def replace_tabs(lines):
+    for line in lines:
+        line.replace('\t', ' ')
+    return lines
+
 def remove_trailing_white_spaces(lines):
     stripped_lines = []
     for line in lines:
-        line.strip(" ")
+        line.strip(' ')
         stripped_lines.append(line)
     return stripped_lines
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("files", nargs='+', help="Input files")
-    parser.add_argument("-c", "--crlf",
-                        help="Use CRLF as newline control character (\r\n)",
-                        default='\n',
-                        action='store_const', const='\r\n')
-    parser.add_argument("-n", "--names",
-                        help="Show name of processed files",
-                        action='store_true')
-    parser.add_argument("-s", "--stats",
-                        help="Show statistics on minified version",
-                        action='store_true')
-    parser.add_argument("-m", "--keep-multiline",
-                        help="Don't strip multiline comments (/* ... */)",
-                        action='store_true')
-    parser.add_argument("-i", "--keep-inline",
-                        help="Do not strip inline comments (// ...)",
-                        action='store_true')
-    parser.add_argument("-w", "--keep-newline",
-                        help="Keep newline control characters",
-                        action='store_true')
-    args = parser.parse_args()
-    return args
 
 
 def minify(filename):
@@ -47,11 +28,15 @@ def minify(filename):
         lines = source.readlines()
 
         lines = remove_blank_lines(lines)
-
+        lines = replace_tabs(lines)
         lines = remove_trailing_white_spaces(lines)
         print lines
         for line in lines:
            print line
+        destination = filename.strip(".c")+"_min.c"
+        dest = open(destination, "w")
+        dest.writelines(lines)
+
 
 
 
